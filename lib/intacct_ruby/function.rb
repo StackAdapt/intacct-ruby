b1eb77efb5888c19b9915a3eaa07388ddc9b2225
nomoonx <nomoonx@gmail.com>
2017-08-30 01:34:42 -0400

    try something new

--- lib/intacct_ruby/function.rb
+++ lib/intacct_ruby/function.rb
@@ -50,7 +50,7 @@ module IntacctRuby
       xml = Builder::XmlMarkup.new
 
       arguments_to_convert.each do |key, value|
-        argument_key = key.to_s.upcase
+        argument_key = key.to_s
 
         xml.tag!(argument_key) do
           xml << argument_value_as_xml(value)
